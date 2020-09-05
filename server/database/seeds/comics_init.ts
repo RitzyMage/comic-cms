2e07c5c66e537d35e1db2cbefc13d50f7a16f56e
u0966152 <u0966152@utah.edu>
2020-09-05 16:48:43 -0600

    updated seeds

--- server/database/seeds/comics_init.ts
+++ server/database/seeds/comics_init.ts
@@ -45,9 +45,75 @@ export async function seed(knex: Knex): Promise<void> {
         "https://www.housepetscomic.com/wp-content/uploads/2020/03/2020-03-18-super-camouflage.png",
       posted: "2020-03-18",
     },
+    {
+      id: 4,
+      title: "This Looks Like A Job For The Intern",
+      transcript:
+        "STRIPE: Jim-jim, for the sake of argument, how far deos spot (superdog)'s super-hearing reach? JIM_JIM: Depends on the writer...",
+      mouseover: "that sounded like YOUR car",
+      image:
+        "https://www.housepetscomic.com/wp-content/uploads/2020/03/2020-03-20-this-looks-like-a-job-for-the-intern.png",
+      image_lowres:
+        "https://www.housepetscomic.com/wp-content/uploads/2020/03/2020-03-20-this-looks-like-a-job-for-the-intern.png",
+      posted: "2020-03-20",
+    },
+    {
+      id: 5,
+      title: "Then Who Was Car",
+      transcript:
+        "STRIPE: Now that Jim-Jim is gone, we can discuss this collection of ugly bowties...",
+      mouseover:
+        "The next fifty comics is just them lampshading the previous comic",
+      image:
+        "https://www.housepetscomic.com/wp-content/uploads/2020/03/2020-03-23-then-who-was-car.png",
+      image_lowres:
+        "https://www.housepetscomic.com/wp-content/uploads/2020/03/2020-03-23-then-who-was-car.png",
+      posted: "2020-03-23",
+    },
+    {
+      id: 6,
+      title: "Well Turn Me Into A Newt",
+      transcript:
+        "JIM-JIM: I don;t get why you're ven so obsessed with this! STRIPE: Just admid that you're spot(superdog)!",
+      mouseover: "Well Turn Me Into A Newt",
+      image:
+        "https://www.housepetscomic.com/wp-content/uploads/2020/03/2020-03-25-well-turn-me-into-a-newt.png",
+      image_lowres:
+        "https://www.housepetscomic.com/wp-content/uploads/2020/03/2020-03-25-well-turn-me-into-a-newt.png",
+      posted: "2020-03-25",
+    },
+    {
+      id: 7,
+      title: "Exactly What Spot Would Have Done",
+      transcript: "JIM-JIM: Okay hahah, seriously now somebody help",
+      mouseover:
+        'you\'ll look up and shout "Save us!"... and I\'ll look down, and whisper "kay."',
+      image:
+        "https://www.housepetscomic.com/wp-content/uploads/2020/03/2020-03-27-exactly-what-spot-would-have-done.png",
+      image_lowres:
+        "https://www.housepetscomic.com/wp-content/uploads/2020/03/2020-03-27-exactly-what-spot-would-have-done.png",
+      posted: "2020-03-27",
+    },
+    {
+      id: 8,
+      title: "Shocking Reveal",
+      transcript:
+        "SPOT: Sorry for taking so long, Jim-Jim! Turns out the movie didn't even have an after-credits sequence! JIM-Jim: Hahaha, that's okay...",
+      mouseover: "to probably never be continued",
+      image:
+        "https://www.housepetscomic.com/wp-content/uploads/2020/03/2020-03-30-shocking-reveal.png",
+      image_lowres:
+        "https://www.housepetscomic.com/wp-content/uploads/2020/03/2020-03-30-shocking-reveal.pngg",
+      posted: "2020-03-30",
+    },
   ]);
 
   await knex("COMICS").where({ id: 1 }).update({ next: 2 });
   await knex("COMICS").where({ id: 2 }).update({ previous: 1, next: 3 });
-  await knex("COMICS").where({ id: 3 }).update({ previous: 2 });
+  await knex("COMICS").where({ id: 3 }).update({ previous: 2, next: 4 });
+  await knex("COMICS").where({ id: 4 }).update({ previous: 3, next: 5 });
+  await knex("COMICS").where({ id: 5 }).update({ previous: 4, next: 6 });
+  await knex("COMICS").where({ id: 6 }).update({ previous: 5, next: 7 });
+  await knex("COMICS").where({ id: 7 }).update({ previous: 6, next: 8 });
+  await knex("COMICS").where({ id: 8 }).update({ previous: 7 });
 }
