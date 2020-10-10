ba315e5a0dd69e7c82b55bb936b0097fe3ca5d71
u0966152 <u0966152@utah.edu>
2020-10-10 08:39:58 -0600

    lazy load event called

--- client/pages/archive.vue
+++ client/pages/archive.vue
@@ -9,13 +9,17 @@
 import { Component, Vue } from "vue-property-decorator";
 import PageLink from "../components/PageLink.vue";
 
+const CHUNK_SIZE = 6;
+
+const LINK_HEIGHT = 140;
+
 @Component({
   name: "ComicArchives",
   components: {
     PageLink
   },
   async asyncData({ $axios }: any) {
-    let pages = await $axios.$get("/images?first=1&last=3");
+    let pages = await $axios.$get(`/images?first=1&last=${CHUNK_SIZE}`);
     return { pages };
   }
 } as any)
@@ -32,8 +36,18 @@ export default class ComicArchives extends Vue {
     return "/comic/" + this.name + "/search";
   }
 
-  constructor() {
-    super();
+  private numLoaded = 6;
+
+  private onScroll(event: Event) {
+    let scrollPos = window.scrollY + window.innerHeight;
+    let numVisible = Math.ceil(scrollPos / LINK_HEIGHT);
+    if (numVisible <= this.numLoaded + CHUNK_SIZE) {
+      console.log("LAZY LOAD!");
+    }
+  }
+
+  mounted() {
+    window.addEventListener("scroll", this.onScroll);
   }
 
   private pages!: Array<{ id: number; image: string; image_lowres: string }>;
