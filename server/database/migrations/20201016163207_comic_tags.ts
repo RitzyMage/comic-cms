36b7c8e9f029b011a5cd90654011cf8bd18ec6d7
u0966152 <u0966152@utah.edu>
2020-10-16 16:40:04 -0600

    add tags and join table

---
+++ server/database/migrations/20201016163207_comic_tags.ts
@@ -0,0 +1,12 @@
+import * as Knex from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  return await knex.schema.createTable("TAGS", (table) => {
+    table.increments();
+    table.string("NAME").notNullable();
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return await knex.schema.dropTable("TAGS");
+}
---
+++ server/database/migrations/20201016163647_comic_tags_join.ts
@@ -0,0 +1,14 @@
+import * as Knex from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  return await knex.schema.createTable("COMIC_TAGS", (table) => {
+    table.integer("COMIC").unsigned();
+    table.integer("TAG").unsigned();
+    table.foreign("COMIC").references("ID").inTable("COMICS");
+    table.foreign("TAG").references("ID").inTable("TAGS");
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable("COMIC_TAGS");
+}
