44910954acc04a405b219354b554e2c35f134e98
u0966152 <u0966152@utah.edu>
2020-11-11 06:08:26 -0700

    hash password

--- server/controllers/AuthController.ts
+++ server/controllers/AuthController.ts
@@ -1,6 +1,11 @@
+import bcrypt from "bcryptjs";
+
+const SALT_ROUNDS = 10;
+
 export default class AuthController {
   async verifyUser(username: string, password: string) {
-    console.log(username, password);
+    let hash = bcrypt.hashSync(password, bcrypt.genSaltSync(SALT_ROUNDS));
+    console.log(username, hash);
     return { token: "TOKEN" };
   }
 }
