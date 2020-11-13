635c57bba73ed8e29d5ec79e2e7240ccfd5f5e30
u0966152 <u0966152@utah.edu>
2020-11-13 07:02:47 -0700

    check password match

--- server/controllers/AuthController.ts
+++ server/controllers/AuthController.ts
@@ -1,11 +1,24 @@
 import bcrypt from "bcryptjs";
+import knex from "knex";
+const connection = knex(require("../knexfile"));
 
 const SALT_ROUNDS = 10;
 
 export default class AuthController {
   async verifyUser(username: string, password: string) {
-    let hash = bcrypt.hashSync(password, bcrypt.genSaltSync(SALT_ROUNDS));
-    console.log(username, hash);
+    let user = await connection
+      .select("hash")
+      .from("users")
+      .where({ username })
+      .first();
+    let match = !!user && bcrypt.compareSync(password, user.hash);
+    console.log(match);
     return { token: "TOKEN" };
+  }
+
+  async addUser(username: string, password: string) {
+    let hash = bcrypt.hashSync(password, bcrypt.genSaltSync(SALT_ROUNDS));
+    let user = { username, hash };
+    await connection("users").insert(user);
   }
 }
