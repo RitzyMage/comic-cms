06c5a3aabac30d57997d08ca59a9c2983d3bc6b2
u0966152 <u0966152@utah.edu>
2020-11-25 15:17:20 -0700

    basic add form

--- client/pages/admin/add.vue
+++ client/pages/admin/add.vue
@@ -1,7 +1,21 @@
 <template>
   <div>
     <h1>Add Comic</h1>
-    <button @click="add">ADD</button>
+    <div class="addForm">
+      <span>Title</span>
+      <input v-model="title" type="text" />
+
+      <span>Image</span>
+      <input type="file" accept="image/x-png,image/gif,image/jpeg" />
+
+      <span>Transcript</span>
+      <textarea v-model="transcript" />
+
+      <span>Mouseover Text</span>
+      <input v-model="mouseover" type="text" />
+
+      <button @click="add">ADD</button>
+    </div>
   </div>
 </template>
 
@@ -17,7 +31,19 @@ export class AddComic extends Vue {
   private add() {
     this.$axios.post("/comic");
   }
+  private transcript = "";
+  private title = "";
+  private mouseover = "";
 }
 
 export default AddComic;
 </script>
+
+<style scoped>
+.addForm {
+  display: flex;
+  flex-direction: column;
+  max-width: 600px;
+  margin: 0 auto;
+}
+</style>
