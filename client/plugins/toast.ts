2596f03bf8b871ac85edee1e8e1ab9479b41d7a2
Avery Green <averyg@utahcounty.gov>
2020-12-07 13:54:15 -0700

    toasts removed from store after timeout

--- client/plugins/toast.ts
+++ client/plugins/toast.ts
@@ -19,16 +19,22 @@ declare module "vuex/types/index" {
   interface Store<S> extends Injected {}
 }
 
+const TOAST_LENGTH = 2000;
+let id = 1;
+
 function addToast(error: boolean, message: string) {
   toastStore.addToast({
     error,
     message,
-    id: 1,
+    id,
   });
+  let createdID = id;
+  ++id;
+  setTimeout(() => toastStore.removeToast(createdID), TOAST_LENGTH);
 }
 
 const ToastPlugin: Plugin = (context, inject) => {
-  inject("NotifyToast", (message: string) => console.log(context, message));
+  inject("NotifyToast", (message: string) => addToast(false, message));
   inject("ErrorToast", (message: string) => addToast(true, message));
 };
 
