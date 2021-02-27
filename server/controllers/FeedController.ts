49d5c5f7dcc1c728e6da514718db7884f270ac46
u0966152 <u0966152@utah.edu>
2021-02-27 13:34:28 -0700

    rss feed formatted properly

--- server/controllers/FeedController.ts
+++ server/controllers/FeedController.ts
@@ -3,24 +3,26 @@ import { DAOFunction, TransactionType, TransactionFunction } from "../dao/DAOFun
 import ComicDAO from "../dao/ComicDAO";
 import { Transaction } from "knex";
 
-let CLIENT_URL = "http://localhost:3311";
-let AUTHOR = "John Doe";
+let TITLE = process.env.COMIC_TITLE ?? "";
+let CLIENT_URL = process.env.APP_URL ?? "";
+let AUTHOR = process.env.COMIC_AUTHOR ?? "";
+let DESCRIPTION = process.env.COMIC_DESCRIPTION ?? "";
+let YEAR = new Date().getFullYear();
 export default class FeedController {
   private getFeedGenerator() {
     return new Feed({
-      title: "Feed Title",
-      description: "This is my personal feed!",
+      title: TITLE,
+      description: DESCRIPTION,
       id: CLIENT_URL,
       link: CLIENT_URL,
       language: "en",
       image: `${CLIENT_URL}/image.png`,
       favicon: `${CLIENT_URL}/favicon.ico`,
-      copyright: `All rights reserved 2013, ${AUTHOR}`,
+      copyright: `All rights reserved ${YEAR}, ${AUTHOR}`,
       feedLinks: {},
       author: {
         name: AUTHOR,
-        email: "johndoe@example.com",
-        link: "https://example.com/johndoe",
+        email: process.env.COMIC_EMAIL,
       },
     });
   }
@@ -32,7 +34,7 @@ export default class FeedController {
         (comic: { id: string; title: string; posted: string }) =>
           feed.addItem({
             title: comic.title,
-            link: `${CLIENT_URL}/${comic.id}`,
+            link: `${CLIENT_URL}/comic/${comic.id}`,
             date: new Date(comic.posted),
           })
       );
