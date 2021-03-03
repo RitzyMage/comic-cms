573609bee18714ecac4523e518dc8fa419ceb231
u0966152 <u0966152@utah.edu>
2021-03-03 15:36:46 -0700

    style header on mobile better

--- client/components/NavHeader.vue
+++ client/components/NavHeader.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="header">
-    <h1 class="title">
-      <nuxt-link to="/" class="title-link">{{ name }}</nuxt-link>
-    </h1>
-    <div class="links">
-      <nuxt-link v-for="link in links" :to="link.href" :key="link.name" class="header-link">
-        {{ link.name }}
-      </nuxt-link>
-      <client-only>
-        <nuxt-link v-if="$auth.loggedIn" to="/admin" class="header-link">Manage Comics</nuxt-link>
-        <span v-if="$auth.loggedIn" class="auth-button header-link" @click="logout">Log out</span>
-        <nuxt-link v-else class="auth-button header-link" to="/admin">Log in</nuxt-link>
-      </client-only>
-    </div>
+    <nuxt-link to="/" class="header-link header-link--title">{{ name }}</nuxt-link>
+    <nuxt-link v-for="link in links" :to="link.href" :key="link.name" class="header-link">
+      {{ link.name }}
+    </nuxt-link>
+    <client-only>
+      <nuxt-link v-if="$auth.loggedIn" to="/admin" class="header-link">Manage Comics</nuxt-link>
+      <a v-if="$auth.loggedIn" class="header-link" @click="logout">Log out</a>
+      <nuxt-link v-else class="header-link" to="/admin">Log in</nuxt-link>
+    </client-only>
   </div>
 </template>
 
@@ -38,17 +34,9 @@ export default class NavHeader extends Vue {
 .header {
   display: flex;
   background-color: $primary;
-}
-
-.links {
   display: flex;
   align-items: center;
   justify-content: space-evenly;
-  width: calc(100% - 200px);
-}
-
-.auth-button {
-  flex-grow: 1;
 }
 
 .header-link {
@@ -68,17 +56,8 @@ export default class NavHeader extends Vue {
   background-color: $primaryLight;
 }
 
-.title {
-  width: 200px;
-  max-width: 200px;
+.header-link--title {
   font-size: 1.2em;
-  margin: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.title-link {
   color: $darkBackground;
 }
 </style>
