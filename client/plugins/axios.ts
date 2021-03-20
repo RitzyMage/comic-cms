10d3fe250e06389f5fe7d0a2f82ba6a6b9fe5709
u0966152 <u0966152@utah.edu>
2021-03-20 08:35:46 -0600

    handle no server error correctly

--- client/plugins/axios.ts
+++ client/plugins/axios.ts
@@ -2,10 +2,14 @@ import { Context } from "@nuxt/types";
 
 export default function({ $axios, error }: Context) {
   $axios.onError(exception => {
-    console.log("axios error handler called");
     let response = exception.response;
+    let message = "server error";
+    console.log({ ...exception });
+    if (!response) {
+      message = `server is not running at ${exception.config.baseURL}${exception.config.url} (method: ${exception.config.method})`;
+    }
     error({
-      message: response?.data.message || "server error",
+      message: response?.data.message || message,
       statusCode: response?.status,
     });
     return Promise.resolve(false);
