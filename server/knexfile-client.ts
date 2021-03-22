32d8c4f569020936529319bb251a92745b633881
Avery Green <averyg@utahcounty.gov>
2021-03-22 08:08:21 -0600

    use admin database connection seperate from client; always use mysql

--- server/knexfile-client.ts
+++ server/knexfile-client.ts
@@ -2,23 +2,12 @@
 
 require("dotenv").config();
 
-let useSqlite = !!process.env.USE_SQLITE;
-
-if (useSqlite) {
-  module.exports = {
-    client: "sqlite3",
-    connection: () => ({
-      filename: "./dev.db",
-    }),
-  };
-} else {
-  module.exports = {
-    client: "mysql",
-    connection: {
-      host: "127.0.0.1",
-      user: process.env.ADMIN_USER,
-      password: process.env.DB_PASS,
-      database: "comics",
-    },
-  };
-}
+module.exports = {
+  client: "mysql",
+  connection: {
+    host: "127.0.0.1",
+    user: process.env.CLIENT_USER,
+    password: process.env.DB_PASS_CLIENT,
+    database: "comics",
+  },
+};
--- server/knexfile.ts
+++ server/knexfile.ts
@@ -1,35 +1,18 @@
 require("dotenv").config();
-let useSqliteDev = !!process.env.USE_SQLITE;
 
-if (useSqliteDev) {
-  module.exports = {
-    client: "sqlite3",
-    connection: {
-      filename: "./dev.db",
-    },
-    migrations: {
-      directory: "./database/migrations/",
-      tableName: "migrations",
-    },
-    seeds: {
-      directory: "./database/seeds/",
-    },
-  };
-} else {
-  module.exports = {
-    client: "mysql",
-    connection: {
-      host: "127.0.0.1",
-      user: process.env.ADMIN_USER,
-      password: process.env.DB_PASS,
-      database: "comics",
-    },
-    migrations: {
-      directory: "./database/migrations/",
-      tableName: "migrations",
-    },
-    seeds: {
-      directory: "./database/seeds/",
-    },
-  };
-}
+module.exports = {
+  client: "mysql",
+  connection: {
+    host: "127.0.0.1",
+    user: process.env.ADMIN_USER,
+    password: process.env.DB_PASS_ADMIN,
+    database: "comics",
+  },
+  migrations: {
+    directory: "./database/migrations/",
+    tableName: "migrations",
+  },
+  seeds: {
+    directory: "./database/seeds/",
+  },
+};
